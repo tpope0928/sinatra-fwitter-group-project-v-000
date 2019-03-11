2db631ab8926997cca7476717d1fd9c9c2d58194
Thomas Pope <tpope0928@gmail.com>
2019-03-11 14:47:37 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,15 @@
 
 ActiveRecord::Schema.define(version: 20190311144056) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.string  "content"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+  end
+
 end
