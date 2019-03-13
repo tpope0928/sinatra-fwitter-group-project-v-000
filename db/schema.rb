7e926540f97d7a50632375a8f6048da3a510b6ec
Thomas Pope <tpope0928@gmail.com>
2019-03-13 15:22:32 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190307221915) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.string  "content"
-    t.integer "user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 20190311144056) do
 
 end
